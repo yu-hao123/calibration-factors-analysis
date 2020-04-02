737d8e277b99e6d65d65649c80fc77792721dd51
yu-hao123 <yuhaowangxia@gmail.com>
2020-04-02 13:35:20 -0300

    feat: add distribution percentage

--- src/calibration_analysis.py
+++ src/calibration_analysis.py
@@ -4,18 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from argparse import ArgumentParser
-
-try:
-    plt.style.use('scienza')
-except OSError:
-    print('Could not find scienza matplotlib style, using default')
+from matplotlib.ticker import PercentFormatter
 
 def normalize_data(df):
     result = df.copy()
-    for column_name in result.columns[2:]:
-        result[column_name] /= result.iloc[:, 1]
+    for column_name in result.columns[1:]:
+        result[column_name] /= result.iloc[:, 0]
         result[column_name] *= 100
-    result.iloc[:, 1] = 100
+    result.iloc[:, 0] = 100
     return result
 
 def save_calibration_factors(file_name, possible_positions, results_json):
@@ -43,14 +39,26 @@ def plot_calibration_factors(file_name, possible_positions):
         calibration_data = json.load(i_handle)
     
     df = pd.DataFrame(
-        {"Cases": case["Case Name"], **case["Calibration Factors"]}
-        for case in calibration_data
+            {**case["Calibration Factors"]}
+            for case in calibration_data
     )
     normalized_df = normalize_data(df)
+    normalized_df = normalized_df.mask(
+            np.isclose(normalized_df.values, 0.0000),
+    )
+    normalized_df.insert(
+            0, 
+            "Cases", 
+            [case["Case Name"] for case in calibration_data]
+    )
     print(normalized_df)
 
-    fig = plt.figure(figsize=(10,6))
+    try:
+        plt.style.use('scienza')
+    except OSError:
+        print('Could not find scienza matplotlib style, using default')
 
+    fig = plt.figure(figsize=(9,6))
     n_rows = len(df.index)
     cmap = plt.cm.nipy_spectral
     cmap_list = [cmap(i) for i in range(cmap.N)]
@@ -58,22 +66,43 @@ def plot_calibration_factors(file_name, possible_positions):
             normalized_df, 
             "Cases",
             color=cmap_list[::int(cmap.N/n_rows)],
-            linewidth=0.75
-            )
-    plt.title("Calibration Factors (SVP)")
+            linewidth=1
+    )
+    plt.title("Calibration Factors (VNI)")
     plt.ylabel("Percentage (%)")
     plt.ylim(-50, 150)
-    plt.legend(loc="lower left", ncol=4)
+    plt.legend(loc="lower left", ncol=4, fontsize="small")
     plt.fill_between(possible_positions, 90, 110, color="#b5e2ff")
     plt.show()
+    plot_calibration_factors_distribution(normalized_df)
+
+
+def plot_calibration_factors_distribution(df):
+    df_dist = (df.iloc[:, 2].values).reshape(-1)
+    df_dist = df_dist[~np.isnan(df_dist)]
+    """
+    plt.hist(
+            df_dist, 
+            weights = np.ones(len(df_dist)) / len(df_dist),
+            bins = np.arange(0, 200, 10)
+    )
+    plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+    plt.show()
+    """
+    p10 = df_dist[(df_dist > 90) & (df_dist < 110)].size / float(df_dist.size)
+    p20 = df_dist[(df_dist > 80) & (df_dist < 120)].size / float(df_dist.size)
+
+    print("Calibration Factors in the +/-10% range: {}".format(p10))
+    print("Calibration Factors in the +/-20% range: {}".format(p20))
 
 def main(args):
     possible_positions = [
-        "Supine Spontaneous - 001",
-        "Supine CPAP4 - 001",
-        "Supine CPAP6 - 001",
-        "Supine CPAP8 - 001",
-        "Supine Spontaneous - 002"
+            "Spontaneous - 001",
+            "CPAP4 - 001",
+            "CPAP6 - 001",
+            "CPAP8 - 001",
+            "CPAP10 - 001",
+            "Spontaneous - 002"
     ]
     with open(args.calibration_results, "r") as i_handle:
         results_json = json.load(i_handle)
@@ -82,13 +111,13 @@ def main(args):
             os.path.dirname(args.calibration_results),
             "CalibrationFactorsData.json"
     )
-    """
+    
     save_calibration_factors(
             calibration_data_file, 
             possible_positions, 
             results_json
     )
-    """
+    
     plot_calibration_factors(
             calibration_data_file,
             possible_positions
@@ -98,6 +127,6 @@ def main(args):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "calibration_results"
+            "calibration_results"
     )
     main(parser.parse_args())
